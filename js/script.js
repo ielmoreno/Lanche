00b0f4820dc5c65c402353e4afe09c21bf13293f
GABRIEL <cg3023028@LAB111M19.IFSP>
2023-06-06 20:49:33 -0300

    iniciando o lanchinho

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const vm = new Vue({
+    el: '#app',
+    data: {
+        inputTipoPao: '',
+        inputSalada: [],
+        inputMolho: [],
+        inputHamburguer: '',
+    },
+    computed: {
+        pao() {
+            switch (this.inputTipoPao) {
+                case 'gergelim':
+                    return [
+                        "imagens/pao_gergelim_superior.png",
+                        "imagens/pao_gergelim_inferior.png"
+                    ]
+                case 'australiano':
+                    return [
+                        "imagens/pao_australiano_superior.png",
+                        "imagens/pao_australiano_inferior.png"
+                    ]
+                default:
+                    return [
+                        "imagens/padrao/pao_superior.png",
+                        "imagens/padrao/pao_inferior.png"
+                    ]
+            }
+        }
+    }
+})
