8439d933c0dc7e9ac876a0b0c9e7213c0f355035
GABRIEL <cg3023028@LAB111M19.IFSP>
2023-06-06 22:31:43 -0300

    Plataforma de pedido de lanche

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const vm = new Vue({
     el: '#app',
     data: {
@@ -6,6 +5,10 @@ const vm = new Vue({
         inputSalada: [],
         inputMolho: [],
         inputHamburguer: '',
+        etapa: 1,
+        inputNome: '',
+        inputEmail: '',
+        novoPedidoAssincrono: null,
     },
     computed: {
         pao() {
@@ -26,37 +29,75 @@ const vm = new Vue({
                         "imagens/padrao/pao_inferior.png"
                     ]
             }
-        }
-    }
-=======
-const vm = new Vue({
-    el: '#app',
-    data: {
-        inputTipoPao: '',
-        inputSalada: [],
-        inputMolho: [],
-        inputHamburguer: '',
-    },
-    computed: {
-        pao() {
-            switch (this.inputTipoPao) {
-                case 'gergelim':
-                    return [
-                        "imagens/pao_gergelim_superior.png",
-                        "imagens/pao_gergelim_inferior.png"
-                    ]
-                case 'australiano':
-                    return [
-                        "imagens/pao_australiano_superior.png",
-                        "imagens/pao_australiano_inferior.png"
-                    ]
+        },
+        alface() {
+            if(this.inputSalada.includes('alface'))
+                return "imagens/alface.png"
+            else
+                return "imagens/padrao/alface.png"
+        },
+        ketchup() {
+            if(this.inputMolho.includes('ketchup'))
+                return "imagens/ketchup.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        mostarda() {
+            if(this.inputMolho.includes('mostarda'))
+                return "imagens/mostarda.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        maionese() {
+            if(this.inputMolho.includes('maionese'))
+                return "imagens/maionese.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        hamburguer() {
+            switch (this.inputHamburguer) {
+                case 'bovino':
+                    return 'imagens/bovino.png'
+                case 'frango':
+                    return 'imagens/frango.png'
+                case 'soja':
+                    return 'imagens/soja.png'
                 default:
-                    return [
-                        "imagens/padrao/pao_superior.png",
-                        "imagens/padrao/pao_inferior.png"
-                    ]
+                    return 'imagens/padrao/hamburguer.png'
+            }
+        }
+    },
+    methods: {
+        fazerPedido() {
+            if (this.inputTipoPao && this.inputHamburguer) {
+                this.etapa = 2
+            } else{
+                alert('Você precisa selecionar pelo menos o pão e o hamburguer')
+            }
+        },
+        confirmarPedido() {
+            if (this.inputNome && this.inputEmail){
+                this.etapa = 3
+                this.novoPedidoAssincrono = setTimeout(() => this.novoPedido(), 7000)
+            } else{
+                alert('Você precisa informar nome e e-mail')
+            }
+        },
+        novoPedido() {
+            this.etapa = 1;
+            this.inputTipoPao = '';
+            this.inputSalada = [];
+            this.inputMolho = [];
+            this.inputHamburguer = '';
+            this.inputNome = '';
+            this.inputEmail = '';
+        }
+    },
+    watch: {
+        etapa(novoValor){
+            if(novoValor == 1){
+                clearTimeout(this.novoPedidoAssincrono);
             }
         }
     }
->>>>>>> de6717774c8c7b85df033d9fbacec9e4702a5492
 })
